ca379dd79547b7dc5e8bd02ce23b245547914ee1
BHeyse <54610650+BHeyse@users.noreply.github.com>
2019-09-10 19:53:09 -0500

    Add files via upload
    Initial Commit... need to further refine

---
+++ ThreeSolidPrinciples/src/Manager.java
@@ -0,0 +1,20 @@
+package threesolid;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
+
+class Manager {
+	IWorker worker;
+
+	public void Manager() {
+
+	}
+	public void setWorker(IWorker w) {
+		worker=w;
+	}
+
+	public void manage() {
+		worker.work();
+	}
+}
--- ThreeSolidPrinciples/src/ThreeSolidMain.java
+++ ThreeSolidPrinciples/src/ThreeSolidMain.java
@@ -40,42 +40,9 @@ public class ThreeSolidMain
  }
 
 // interface segregation principle - good example
-interface IWorker {
-	public void work();
-	public void eat();
-}   
+  
 
-class Worker implements IWorker{
-	public void work() {
-		// ....working
-	}
 
-	public void eat() {
-		//.... eating in launch break
-	}
-}
 
-class SuperWorker implements IWorker{
-	public void work() {
-		//.... working much more
-	}
 
-	public void eat() {
-		//.... eating in launch break
-	}
-}
 
-class Manager {
-	IWorker worker;
-
-	public void Manager() {
-
-	}
-	public void setWorker(IWorker w) {
-		worker=w;
-	}
-
-	public void manage() {
-		worker.work();
-	}
-}
